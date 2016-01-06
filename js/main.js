98c3f3335177fb271898e9bf2d8fb533063ca1bc
codyjhsieh <codyjhsieh@gmail.com>
2016-01-05 17:17:50 -0700

    Trying to fix contact form

--- js/main.js
+++ js/main.js
@@ -91,15 +91,17 @@ jQuery(document).ready(function() {
     /* ---------------------------------------------- /*
      * E-mail validation
     /* ---------------------------------------------- */
+    /* ---------------------------------------------- /*
+         * E-mail validation
+        /* ---------------------------------------------- */
     function isValidEmailAddress(emailAddress) {
         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
         return pattern.test(emailAddress);
-    }
-    ;
+    };
 
     /* ---------------------------------------------- /*
-     * Contact form ajax
-    /* ---------------------------------------------- */
+         * Contact form ajax
+        /* ---------------------------------------------- */
 
     jQuery('#contact-form').submit(function (e) {
 
@@ -120,13 +122,16 @@ jQuery(document).ready(function() {
             response.fadeIn(500);
             response.html('<i class="fa fa-warning"></i> Please fix the errors and try again.');
         }
-
-        else {
+ else {
             jQuery.ajax({
-                type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-                url: 'php/contact.php', // the url where we want to POST
-                data: formData, // our data object
-                dataType: 'json', // what type of data do we expect back from the server
+                type: 'POST',
+                // define the type of HTTP verb we want to use (POST for our form)
+                url: 'php/contact.php',
+                // the url where we want to POST
+                data: formData,
+                // our data object
+                dataType: 'json',
+                // what type of data do we expect back from the server
                 encode: true,
                 success: function (res) {
                     var ret = jQuery.parseJSON(JSON.stringify(res));
@@ -136,6 +141,7 @@ jQuery(document).ready(function() {
         }
         return false;
     });
-    
+
+
     
 });

fdc6b0823b3e79a8c2d0261054167b609f0c3258
Cody Hsieh <cohsieh@berkeley.edu>
2018-02-09 23:32:34 -0800

    Fix jquery issues

--- source/js/script.js
+++ source/js/script.js
@@ -1,62 +1,99 @@
 // console.log('hi');
-$(document).ready(function(){
+$(document).ready(function() {
 
-   var newHash      = "",
-     $mainContent = $("#main-content"),
-     $el;
+    $(".listItem").hide();
 
-   //Navigation Handler
-   $(".navLink").click(function(){
-      window.location.hash = $(this).attr("href");
-      return false;
-   });
-
-   $(window).bind('hashchange', function(){
-
-     newHash = window.location.hash.substring(1);
-     
-     if (newHash) {
-         $mainContent
-             .find(".exampleList")
-             .fadeOut(200, function() {
-                 $mainContent.hide().load(newHash + " .exampleList", function() {
-                     $mainContent.fadeIn(200);
-                     $("a.navLink").removeClass('selected')
-                     $("a.navLink[href='"+newHash+"']").addClass('selected')
-                 });
-             });
-     };
-     
-   });
-    
-   $(window).trigger('hashchange');
-
-   //Color background scroll event
-   $(".photoScroll").scroll(function(){
-   if ($(this).scrollLeft() > 2000) {
-      $('#rectangle').addClass('colorFive')
-      $('#rectangle').removeClass('colorFour')
-   }
-   else if ($(this).scrollLeft() > 1700) {
-      $('#rectangle').addClass('colorFour')
-      $('#rectangle').removeClass('colorThree')
-      $('#rectangle').removeClass('colorFive')
-   }
-   else if ($(this).scrollLeft() > 1100) {
-      $('#rectangle').addClass('colorThree')
-      $('#rectangle').removeClass('colorTwo')
-      $('#rectangle').removeClass('colorFour')
-   }
-   else if ($(this).scrollLeft() > 300) {
-      $('#rectangle').addClass('colorTwo')
-      $('#rectangle').removeClass('colorThree')
-      $('#rectangle').removeClass('colorOne')
-   }
-   else if ($(this).scrollLeft() > 0) {
-      $('#rectangle').addClass('colorOne')
-      $('#rectangle').removeClass('colorTwo')
-   }
-   });
+    var newHash = "",
+        $mainContent = $("#main-content"),
+        $el;
 
 
+    function colorHandler(scrollLeft) {
+
+    }
+
+    //Navigation Handler
+    $(".navLink").click(function() {
+        window.location.hash = $(this).attr("href").slice(0, -5);
+        return false;
+    });
+
+    $(window).bind('hashchange', function() {
+
+        newHash = window.location.hash.substring(1);
+
+        if (newHash) {
+            $("a.navLink").removeClass('selected')
+            $("a.navLink[href='" + newHash + ".html']").addClass('selected')
+
+            $mainContent.find(".exampleList").fadeOut(500, function() {
+                $mainContent.hide().load(newHash + ".html .exampleList", function() {
+                    $mainContent.find(".exampleList").scroll(function() {
+                        console.log($(this).scrollLeft());
+                        if ($(this).scrollLeft() > 2000) {
+                            $('#rectangle').addClass('colorFive')
+                            $('#rectangle').removeClass('colorFour')
+                        } else if ($(this).scrollLeft() > 1700) {
+                            $('#rectangle').addClass('colorFour')
+                            $('#rectangle').removeClass('colorThree')
+                            $('#rectangle').removeClass('colorFive')
+                        } else if ($(this).scrollLeft() > 1100) {
+                            $('#rectangle').addClass('colorThree')
+                            $('#rectangle').removeClass('colorTwo')
+                            $('#rectangle').removeClass('colorFour')
+                        } else if ($(this).scrollLeft() > 300) {
+                            $('#rectangle').addClass('colorTwo')
+                            $('#rectangle').removeClass('colorThree')
+                            $('#rectangle').removeClass('colorOne')
+                        } else if ($(this).scrollLeft() > 0) {
+                            $('#rectangle').addClass('colorOne')
+                            $('#rectangle').removeClass('colorTwo')
+                        }
+                    });
+                    $mainContent.fadeIn(0, function() {
+                        $(".listItem").hide();
+                        $(".listItem").each(function(i) {
+                            $(this).delay((i + 1) * 250).fadeIn();
+                        });
+                        console.log($mainContent.find(".exampleList"));
+
+                    });
+                });
+                // $mainContent.hide();
+            });
+            console.log(newHash + ".html .exampleList");
+        };
+
+    });
+
+    $(window).trigger('hashchange');
+
+    newHash = window.location.hash.substring(1);
+    //Color background scroll event
+    $(".exampleList").scroll(function() {
+        if ($(this).scrollLeft() > 2000) {
+            $('#rectangle').addClass('colorFive')
+            $('#rectangle').removeClass('colorFour')
+        } else if ($(this).scrollLeft() > 1700) {
+            $('#rectangle').addClass('colorFour')
+            $('#rectangle').removeClass('colorThree')
+            $('#rectangle').removeClass('colorFive')
+        } else if ($(this).scrollLeft() > 1100) {
+            $('#rectangle').addClass('colorThree')
+            $('#rectangle').removeClass('colorTwo')
+            $('#rectangle').removeClass('colorFour')
+        } else if ($(this).scrollLeft() > 300) {
+            $('#rectangle').addClass('colorTwo')
+            $('#rectangle').removeClass('colorThree')
+            $('#rectangle').removeClass('colorOne')
+        } else if ($(this).scrollLeft() > 0) {
+            $('#rectangle').addClass('colorOne')
+            $('#rectangle').removeClass('colorTwo')
+        }
+    });
+});
+$(window).on('load', function() {
+    $(".listItem").each(function(i) {
+        $(this).delay((i + 1) * 250).fadeIn();
+    });
 });
